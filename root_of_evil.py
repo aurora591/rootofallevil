22ecbcd91a64e719fb1b2476129bdc18e0845e62
aurora591 <145192284+aurora591@users.noreply.github.com>
2025-06-25 15:00:20 +0300

    fixed code2

--- root_of_evil.py
+++ root_of_evil.py
@@ -1,13 +1,20 @@
 def sqr(num):
-    if not str(num).isdigit():
-        raise TypeError("not expects not digit input")
-    if num < 0:
-        raise ValueError("not expects negative int")
-    return num ** 0.5
+    """
+    this function gets a number and return his sqrt if he is a number and not negative
+    :param num: int of number
+    :return: sqrt of the num
+    """
+    try:
+        num ** 0.5
+    except TypeError:
+        print("not expects not digit input")
+    else:
+        if num < 0:
+            raise ValueError("not expects negative int")
+        return num ** 0.5
 
 
 def main():
-    print(sqr(9))
+    print(sqr(-9))
 if __name__ == "__main__":
     main()
-
